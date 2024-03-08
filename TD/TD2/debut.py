57986a29cd67cdd9465be8227525eb887dcc0c1a
Ayan SARKAR <ayan.sarkar@ens.uvsq.fr>
2024-03-08 15:24:25 +0100

    fin 1)

---
+++ TD/TD2/debut.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+CANVAS_WIDTH, CANVAS_HEIGHT = 600, 400
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
+
+# Début de votre code
+x = CANVAS_WIDTH / 2
+y0 = 100
+y1 = CANVAS_HEIGHT - 100
+canvas.create_line(x, y0, x, y1)
+canvas.create_oval(x - 50, y0 - 50, x + 50, y0 + 50)
+canvas.create_oval(x - 50, y1 - 50, x + 50, y1 + 50)
+canvas.create_oval(x - 50, (y0 + y1) / 2 - 50, x + 50, (y0 + y1) / 2 + 50)
+
+# Fin de votre code
+
+canvas.grid()
+root.mainloop()
